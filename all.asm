5718f7ff077fb21e5c9f38c9d6c7768c9020c0c1
Alex Corley <alexcorley2011@hotmail.com>
2024-03-05 19:07:04 -0500

    Update all.asm
    Forgot to update in previous commit

--- all.asm
+++ all.asm
@@ -16,126 +16,144 @@ org $008000			;dummy org so functions work
 	incsrc mmio.asm
 	incsrc audio_constants.asm
 
-check bankcross off
 org $C00000
-	incsrc bank_C0.asm
+check bankcross half
+	incsrc "bank_C0.asm"
+	warnpc $C07FFC
+	padbyte $00
+	pad $C07FFC
+if !version == 1
+	db $19, $3F, $AA, $C3
+else
+	db $AA, $C3, $3F, $19
+endif
 org $808000
-	incsrc bank_80.asm
+	incsrc "bank_80.asm"
 org $C10000
-	incsrc bank_C1.asm
-;org $C20000
-	incsrc bank_C2.asm
-;org $C30000
-	incsrc bank_C3.asm
-;org $C40000
-	incsrc bank_C4.asm
-;org $C50000
-	incsrc bank_C5.asm
-;org $C60000
-	incsrc bank_C6.asm
-;org $C70000
-	incsrc bank_C7.asm
-;org $C80000
-	incsrc bank_C8.asm
-;org $C90000
-	incsrc bank_C9.asm
-;org $CA0000
-	incsrc bank_CA.asm
-;org $CB0000
-	incsrc bank_CB.asm
-;org $CC0000
-	incsrc bank_CC.asm
-;org $CD0000
-	incsrc bank_CD.asm
-;org $CE0000
-	incsrc bank_CE.asm
-;org $CF0000
-	incsrc bank_CF.asm
-;org $D00000
-	incsrc bank_D0.asm
-;org $D10000
-	incsrc bank_D1.asm
-;org $D20000
-	incsrc bank_D2.asm
-;org $D30000
-	incsrc bank_D3.asm
-;org $D40000
-	incsrc bank_D4.asm
-;org $D50000
-	incsrc bank_D5.asm
-;org $D60000
-	incsrc bank_D6.asm
-;org $D70000
-	incsrc bank_D7.asm
-;org $D80000
-	incsrc bank_D8.asm
-;org $D90000
-	incsrc bank_D9.asm
-;org $DA0000
-	incsrc bank_DA.asm
-;org $DB0000
-	incsrc bank_DB.asm
-;org $DC0000
-	incsrc bank_DC.asm
-;org $DD0000
-	incsrc bank_DD.asm
-;org $DE0000
-	incsrc bank_DE.asm
-
-check bankcross full
-
-;org $DEA932
-	incsrc graphics_part_1.asm
-
-
+	check bankcross full
+	incsrc "bank_C1-ED.asm"
+	padbyte $00
+	pad $EE0000
 org $EE0000
-	incsrc sound.asm	;sound
-
+	incsrc "bank_EE-F2.asm"
+	padbyte $00
+	pad $F30000
 org $F30000
-	incsrc bank_F3.asm	;graphics_part_2
+	check bankcross half
+	incsrc "bank_F3.asm"
+	padbyte $00
+	pad $F38000
 org $B38000
-	incsrc bank_B3.asm	;sprite main code
+	incsrc "bank_B3.asm"
+	padbyte $00
+	pad $B40000
 org $F40000
-	incsrc bank_F4.asm	;graphics_part_3
+	incsrc "bank_F4.asm"
+	padbyte $00
+	pad $F48000
 org $B48000
-	incsrc bank_B4.asm
+	incsrc "bank_B4.asm"
+	padbyte $00
+	pad $B50000
 org $F50000
-	incsrc bank_F5.asm	;graphics_part_4
+if !version == 0
+	db $32, $02, $93, $12
+else
+	db $02, $12, $93, $32
+endif
+	incsrc "bank_F5.asm"
+	padbyte $00
+	pad $F58000
 org $B58000
-	incsrc bank_B5.asm
+	incsrc "bank_B5.asm"
+	padbyte $00
+	pad $B60000
 org $F60000
-	incsrc bank_F6.asm	;graphics_part_5
+	incsrc "bank_F6.asm"
+	padbyte $00
+	pad $F68000
 org $B68000
-	incsrc bank_B6.asm	;bosses
+	incsrc "bank_B6.asm"
+	padbyte $00
+	pad $B70000
 org $F70000
-	incsrc bank_F7.asm	;text
+	check bankcross full
+	incsrc "bank_F7.asm"
+	padbyte $00
+	pad $F80000
 org $F80000
-	incsrc bank_F8.asm	;graphics_part_6
+	check bankcross half
+	incsrc "bank_F8.asm"
+	padbyte $00
+	pad $F88000
 org $B88000
-	incsrc bank_B8.asm
+DATA_B88000:
+if !version == 0
+	db $12, $29, $DE, $B3
+else
+	db $B3, $DE, $12, $29
+endif
+	incsrc "bank_B8.asm"
+	padbyte $00
+	pad $B90000
 org $F90000
-	incsrc bank_F9.asm	;animation/secret ending layer 2
+	check bankcross full
+	incsrc "bank_F9.asm"
+	warnpc $F9D000
+	padbyte $00
+	pad $F9D000
 org $B9D000
-	incsrc bank_B9.asm
+	incsrc "bank_B9.asm"
+	padbyte $00
+	pad $BA0000
 org $FA0000
-	incsrc bank_FA.asm	;graphics_part_7
-org $BA8000
-	incsrc bank_BA.asm
+	incsrc "bank_FA.asm"
+	warnpc $FA9000
+	padbyte $00
+	pad $FA9000
+org $BA9000
+	incsrc "bank_BA.asm"
+	padbyte $00
+	pad $BB0000
 org $FB0000
-	incsrc bank_FB.asm	;graphics_part_8
+	check bankcross half
+	incsrc "bank_FB.asm"
+	padbyte $00
+	pad $FB8000
 org $BB8000
-	incsrc bank_BB.asm
+	incsrc "bank_BB.asm"
+	padbyte $00
+	pad $BBFFFC
+if !version == 0
+	db $00, $AA, $AC, $3C
+else
+	db $3C, $AC, $AA, $00
+endif
 org $FC0000
-	incsrc bank_FC.asm
+	incsrc "bank_FC.asm"
+	padbyte $00
+	pad $FC8000
 org $BC8000
-	incsrc bank_BC.asm
+	incsrc "bank_BC.asm"
+	padbyte $00
+	pad $BD0000
 org $FD0000
-	incsrc bank_FD.asm
+	check bankcross full
+	incsrc "bank_FD.asm"
+	padbyte $00
+	pad $FE0000
 org $FE0000
-	incsrc bank_FE.asm
+	incsrc "bank_FE.asm"
+	warnpc $FEB800
+	padbyte $00
+	pad $FEB800
 org $BEB800
-	incsrc bank_BE.asm
+	incsrc "bank_BE.asm"
+	padbyte $00
+	pad $BF0000
 org $FF0000
-	incsrc bank_FF.asm
+	incsrc "bank_FF.asm"
+	padbyte $00
+	pad $FFFFFF
 org $FFFFFF
 	db $00
